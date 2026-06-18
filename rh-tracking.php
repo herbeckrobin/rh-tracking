e3d2f55bd4b7cac6eba7d5a253be45f5aac41fe4
Robin Herbeck <info@robinherbeck.com>
2026-06-18 10:33:25 +0200

    Switch aus dem Core nutzen statt lokaler Kopie

--- rh-tracking.php
+++ rh-tracking.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/herbeckrobin/rh-tracking
  * Update URI:        https://github.com/herbeckrobin/rh-tracking
  * Description:       Cookieless Analytics für WordPress: Umami, Plausible, Matomo und GoatCounter, jeder als eigene Anbieter-Reihe. DSGVO-freundlich. Teil der rh-blueprint Kollektion.
- * Version:           0.2.0
+ * Version:           0.2.1
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Robin Herbeck
@@ -21,7 +21,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('RHTRACKING_VERSION', '0.2.0');
+define('RHTRACKING_VERSION', '0.2.1');
 define('RHTRACKING_PLUGIN_FILE', __FILE__);
 define('RHTRACKING_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('RHTRACKING_PLUGIN_URL', plugin_dir_url(__FILE__));
